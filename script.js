36ef9f7dfa33efc4c201600ea1ecba0c51d6581d
Khaayz <117873115+Khairulajisyafii@users.noreply.github.com>
2024-11-28 22:48:44 +0700

    Update Porto

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script ini bisa digunakan untuk interaksi di halaman jika diperlukan
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Halaman Portofolio Khairul Aji Syafi’i telah dimuat.");
+});
