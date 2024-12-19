3d00faf6e581045618b94a0021702eb734046860
Khaayz <117873115+Khairulajisyafii@users.noreply.github.com>
2024-12-19 22:01:28 +0700

    BigUpdates

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-// Script ini bisa digunakan untuk interaksi di halaman jika diperlukan
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Halaman Portofolio Khairul Aji Syafi’i telah dimuat.");
 });
+function login() {
+  let cek = 0;
+  var nama = prompt("Masukan nama panggilan  anda Maksimal 7 karakter");
+  if (nama.length > 7) {
+    nama = "Anonim";
+    cek = 1;
+    alert("Nama melebihi Jumlah karakter,gagal login");
+  } else if (nama.length <= 0) {
+    nama = "Anonim";
+    cek = 1;
+    alert("Nama kosong,gagal login");
+  }
+  if (cek == 0) {
+    var status = prompt(
+      "Masukkan status Anda(pelajar dsb) maksimal 10 karakter"
+    );
+    if (status.length > 10) {
+      status = "Undefined";
+      cek = 1;
+      alert("Status melebihi Jumlah karakter,gagal login");
+    } else if (status.length <= 0) {
+      status = "Undefined";
+      cek = 1;
+      alert("Status kosong,gagal login");
+    }
+    ubah();
+  }
+  function ubah() {
+    document.getElementById("Name").innerHTML = nama;
+    document.getElementById("Status").innerHTML = status;
+  }
+}
